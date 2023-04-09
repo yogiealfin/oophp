a581770824218264514c895f4352720128951df1
yogiealfin <yogiealfin23@gmail.com>
2023-04-09 12:05:50 +0700

    Object Type

---
+++ object-type.php
@@ -0,0 +1,36 @@
+<?php
+
+class Produk
+{
+    public $judul, $penulis, $penerbit, $harga;
+
+    public function __construct($judul, $penulis, $penerbit, $harga)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+}
+
+class cetakInfoProduk
+{
+    public function cetak(Produk $produk)
+    {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp.{$produk->harga})";
+        return $str;
+    }
+}
+
+$produk1 = new Produk("One Piece", "Eichiro Oda", "Shonen Jump", 30000);
+$infoProduk1 = new cetakInfoProduk;
+echo $infoProduk1->cetak($produk1);
+
+
+// echo $produk1->getLabel();
+// $produk2 = new Produk();
