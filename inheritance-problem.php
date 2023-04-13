af4266c5b92df3d936216d0f83bd4be55707c557
yogiealfin <yogiealfin23@gmail.com>
2023-04-13 13:58:59 +0700

    Inheritance 1

---
+++ inheritance-problem.php
@@ -0,0 +1,53 @@
+<?php
+
+class Produk
+{
+    public $judul, $penulis, $penerbit, $harga, $jmlHalaman, $waktuMain, $tipe;
+
+    public function __construct($judul, $penulis, $penerbit, $harga, $jmlHalaman, $waktuMain, $tipe)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+        $this->jmlHalaman = $jmlHalaman;
+        $this->waktuMain = $waktuMain;
+        $this->tipe = $tipe;
+    }
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoLengkap()
+    {
+        $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        if ($this->tipe == "Komik") {
+            $str .= " - {$this->jmlHalaman} Halaman.";
+        } else if ($this->tipe == "Game") {
+            $str .= " ~ {$this->waktuMain} Jam.";
+        }
+
+        return $str;
+    }
+}
+
+
+class cetakInfoProduk
+{
+    public function cetak(Produk $produk)
+    {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp.{$produk->harga})";
+        return $str;
+    }
+}
+
+$produk1 = new Produk("One Piece", "Eichiro Oda", "Shonen Jump", 30000, 200, 0, "Komik");
+$produk2 = new Produk("Uncharted", "Neill Druckmann", "Sony Computer Entertaintmen", 429000, 0, 50, "Game");
+// $infoProduk1 = new cetakInfoProduk;
+// echo $infoProduk1->cetak($produk1);
+
+
+echo $produk1->getInfoLengkap();
+// $produk2 = new Produk();
